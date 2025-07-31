94a2c3cefafe81467c27a6c4bd00fefc38527457
ldy <ldy4869@gmail.com>
2025-07-31 20:47:39 +0900

    화면 왜곡 수정

--- Assets/Scripts/Dialog/DialogForm.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class DialogForm : MonoBehaviour
-{
-    public int objectID;                // ex: NPC ID
-    [TextArea(3, 5)]
-    public string[] lines;              // 해당 NPC의 대사 목록
-}
---
+++ Assets/Scripts/RenderManage.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class RenderManage : MonoBehaviour
+{
+    public FullScreenPassRendererFeature render;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        render.SetActive(false);
+    }
+
+   
+}
