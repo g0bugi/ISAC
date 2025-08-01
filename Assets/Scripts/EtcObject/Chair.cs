2c9755967f1265752c4fb6ffbade154af6824efa
ldy <ldy4869@gmail.com>
2025-08-01 20:41:03 +0900

    d

--- Assets/Scripts/EtcObject/Chair.cs
+++ Assets/Scripts/EtcObject/Chair.cs
@@ -5,7 +5,16 @@ public class Chair : MonoBehaviour, IInteractiable
     public GameObject player;
     public void Action()
     {
-        player.transform.position = transform.position;
-        Debug.Log("check");
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.MovePosition(transform.position + Vector3.up * 0.5f); // 약간 위로 띄워서 충돌 방지
+        }
+        else
+        {
+            player.transform.position = transform.position + Vector3.up * 0.5f;
+        }
+
+        Debug.Log("check: moved player");
     }
 }
