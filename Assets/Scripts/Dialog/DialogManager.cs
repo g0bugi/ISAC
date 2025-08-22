c24bac379823e337d6ebaa391d33add733dde513
ldy <ldy4869@gmail.com>
2025-08-22 19:45:55 +0900

    엔드씬 간다

--- Assets/Scripts/Dialog/DialogManager.cs
+++ Assets/Scripts/Dialog/DialogManager.cs
@@ -98,7 +98,7 @@ public class DialogManager : MonoBehaviour
         // 현재 대화 ID에 해당하는 모든 대화 내용이 끝났는지 확인합니다.
         if (talkIndex == talkData[id].Length)
         {
-            if(scanObject.name == "GuitarManAnimat02022")
+            if(scanObject.name == "GuitarMan")
             StartCoroutine(ChangeSceneWithFadeOut());
             // 대화가 종료되면 isAction 플래그를 false로 설정하고 인덱스를 초기화합니다.
             isAction = false;
