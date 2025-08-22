fee7d616a5d9c299499b8ccb0757b8915c81a92a
ldy <ldy4869@gmail.com>
2025-08-22 18:08:46 +0900

    tv 업데이트

--- Assets/Scripts/Television.cs
+++ Assets/Scripts/Television.cs
@@ -5,14 +5,21 @@ public class Television : MonoBehaviour, IInteractiable
 {
     public VideoPlayer player;
     bool IsPlay = false;
+    public AudioSource noise;
    public void Action()
     {
         Debug.Log("Æ¼ºñ´Ù~~");
        IsPlay = !IsPlay;
         if (IsPlay)
+        {
             TurnOn();
+            noise.Play();
+        }
         else
+        {
             TurnOff();
+            noise.Stop();
+        }
     }
 
     private void TurnOn()
